c07c01e07a1699fa9c4e38207a7063c260d8def9
busenurkutluca <busenurkutluca@gmail.com>
2024-09-17 21:36:47 +0300

    hafta1gun2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,59 @@
 package org.example;
 
+import static java.lang.Integer.reverse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static boolean isPalindrome(int number) {
+        int originalNumber = Math.abs(number);
+        int reversedNumber = 0;
+        int temp = originalNumber;
+
+        // Sayıyı ters çevirme işlemi
+        while (temp > 0) {
+            int lastDigit = temp % 10; // Son basamağı alıyoruz
+            reversedNumber = (reversedNumber * 10) + lastDigit; // Sayıyı ters çeviriyoruz
+            temp /= 10; // Bir basamak küçültüyoruz
+        }
+
+        // Ters çevrilmiş sayıyla orijinal sayıyı karşılaştırıyoruz
+        return originalNumber == reversedNumber;
     }
+
+
+    public static boolean isPerfectNumber(int number) {
+        // 0'dan küçük sayılar mükemmel sayı olamaz
+        if (number < 1) {
+            return false;
+        }
+
+        int sumOfDivisors = 0;
+
+        // 1'den başlayarak, sayının tüm bölenlerini bulup topluyoruz
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) { // Sayının böleni mi?
+                sumOfDivisors += i; // Bölenini toplama ekle
+            }
+        }
+
+        // Bölenlerin toplamı sayının kendisine eşit mi?
+        return sumOfDivisors == number;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
